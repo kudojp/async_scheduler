6470d81ebf8b1b69a602154178e833bf872a4317
kudojp <heyjudejudejude1968@gmail.com>
2022-01-01 23:46:02 +0900

    Add method doc of #io_read

--- lib/async_scheduler/scheduler.rb
+++ lib/async_scheduler/scheduler.rb
@@ -111,6 +111,14 @@ module AsyncScheduler
       Fiber.yield
     end
 
+    # Invoked by IO#read to read length bytes from io into a specified buffer (see IO::Buffer).
+    # The length argument is the “minimum length to be read”. If the IO buffer size is 8KiB, but the length is 1024 (1KiB), up to 8KiB might be read, but at least 1KiB will be.
+    # Generally, the only case where less data than length will be read is if there is an error reading the data.
+    # Specifying a length of 0 is valid and means try reading at least once and return any available data.
+
+    # Suggested implementation should try to read from io in a non-blocking manner and call io_wait if the io is not ready (which will yield control to other fibers).
+    # See IO::Buffer for an interface available to return data.
+    # Expected to return number of bytes read, or, in case of an error, -errno (negated number corresponding to system's error code).
     def io_read(io, buffer, length) # read length or -errno
     end
 
