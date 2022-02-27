5b0eb7a8705a3188287f3f17d93f47955ec07dbe
kudojp <heyjudejudejude1968@gmail.com>
2022-02-27 21:34:56 +0900

    Add test cases to timeout_spec

--- spec/blockings/timeout_spec.rb
+++ spec/blockings/timeout_spec.rb
@@ -24,17 +24,56 @@ RSpec.describe AsyncScheduler do
     end
 
     context "when Timeout.timeout is called with duration which is shorter than the block execution time" do
-      it "raises an error as specified with arguments" do
+      context "when block execution time is eternal" do
+        it "raises an error as specified with arguments" do
+          thread = Thread.new do
+            Fiber.set_scheduler AsyncScheduler::Scheduler.new
+            Fiber.schedule do
+              t = Time.now
+              timeout_error_message = "timeout!"
+              expect{
+                Timeout.timeout(0.5, TimeoutError, timeout_error_message) do
+                  sleep()
+                end
+              }.to raise_error(TimeoutError, timeout_error_message)
+            end
+          end
+          thread.join
+        end
+      end
+
+      context "when block execution time is specified" do
+        it "raises an error as specified with arguments" do
+          thread = Thread.new do
+            Fiber.set_scheduler AsyncScheduler::Scheduler.new
+            Fiber.schedule do
+              t = Time.now
+              timeout_error_message = "timeout!"
+              expect{
+                Timeout.timeout(0.5, TimeoutError, timeout_error_message) do
+                  sleep(1)
+                end
+              }.to raise_error(TimeoutError, timeout_error_message)
+            end
+          end
+          thread.join
+        end
+      end
+    end
+
+    context "when Timeout.timeout is called without duration" do
+      it "returns the return value of the block" do
         thread = Thread.new do
           Fiber.set_scheduler AsyncScheduler::Scheduler.new
           Fiber.schedule do
             t = Time.now
             timeout_error_message = "timeout!"
-            expect{
-              Timeout.timeout(0.5, TimeoutError, timeout_error_message) do
-                sleep(1)
+            expect(
+              Timeout.timeout(nil, TimeoutError, timeout_error_message) do
+                sleep(0.5)
+                break 12
               end
-            }.to raise_error(TimeoutError, timeout_error_message)
+            ).to eq(12)
           end
         end
         thread.join
