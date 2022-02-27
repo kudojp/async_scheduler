cbc498f2eb81dc12262b7b077334d9c97589a59e
kudojp <heyjudejudejude1968@gmail.com>
2022-02-27 20:49:03 +0900

    Add spec for timeout_after

---
+++ spec/blockings/timeout_spec.rb
@@ -0,0 +1,44 @@
+# frozen_string_literal: true
+require 'timeout'
+class TimeoutError < StandardError; end
+
+RSpec.describe AsyncScheduler do
+  describe "Timeout.timeout() called asynchronously" do
+    context "when Timeout.timeout is called with duration which is longer than the block execution time" do
+      it "returns the return value of the block" do
+        thread = Thread.new do
+          Fiber.set_scheduler AsyncScheduler::Scheduler.new
+          Fiber.schedule do
+            t = Time.now
+            timeout_error_message = "timeout!"
+            expect(
+              Timeout.timeout(1, TimeoutError, timeout_error_message) do
+                sleep(0.5)
+                break 12
+              end
+            ).to eq(12)
+          end
+        end
+        thread.join
+      end
+    end
+
+    context "when Timeout.timeout is called with duration which is shorter than the block execution time" do
+      it "raises an error as specified with arguments" do
+        thread = Thread.new do
+          Fiber.set_scheduler AsyncScheduler::Scheduler.new
+          Fiber.schedule do
+            t = Time.now
+            timeout_error_message = "timeout!"
+            expect{
+              Timeout.timeout(0.5, TimeoutError, timeout_error_message) do
+                sleep(1)
+              end
+            }.to raise_error(TimeoutError, timeout_error_message)
+          end
+        end
+        thread.join
+      end
+    end
+  end
+end
