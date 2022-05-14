e2bd6edff4dc56282b6c620d2d195ed411f6eb7d
kudojp <heyjudejudejude1968@gmail.com>
2022-05-14 16:41:59 +0900

    Add spec to test ip resolved by Scheduler#address_resolve is the same as that of Socket.getaddrinfo

--- spec/blockings/address_resolve_spec.rb
+++ spec/blockings/address_resolve_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe AsyncScheduler do
     puts "###### TODO! Confirm that times below does not improve in a liner manner."
 
     t = Time.now
-    params = [1, 10, 100]
+    params = [1, 10, 100, 1000]
 
     params.each do |num|
       t = Time.now
@@ -22,5 +22,20 @@ RSpec.describe AsyncScheduler do
       thread.join
       puts "#{Time.now - t} seconds for resolving #{num} hostname."
     end
+  end
+
+  it "resolves an address into IP and the result is the same as that in a synchronous manner " do
+    thread_resolving_address = Thread.new do
+      Fiber.set_scheduler AsyncScheduler::Scheduler.new
+
+      ips = nil
+      Fiber.schedule do
+        ips = Socket.getaddrinfo("www.google.com", 443)
+      end
+      ips
+    end
+
+    # NOTE: This test case could be flaky, because IP addresses mapped from "www.google.com" could change.
+    expect(thread_resolving_address.value).to contain_exactly Socket.getaddrinfo("www.google.com", 443)
   end
 end
