8997bb86986dd10ac45fc93e1a165d106c461daf
kudojp <heyjudejudejude1968@gmail.com>
2022-12-12 19:24:19 +0900

    Rename OriginalThreadAlreadySetError to BelongingThreadAlreadySetError

--- lib/async_scheduler/cross_thread_usage_detector.rb
+++ lib/async_scheduler/cross_thread_usage_detector.rb
@@ -1,10 +1,10 @@
 module CrossThreadUsageDetector
-  class OriginalThreadAlreadySetError < StandardError; end
+  class BelongingThreadAlreadySetError < StandardError; end
   class CrossThreadUsageError < StandardError; end
 
   def set_belonging_thread(original_thread_object_id)
     if defined? @belonging_thread_object_id
-      raise OriginalThreadAlreadySetError.new("@belonging_thread_object_id is already set with #{@belonging_thread_object_id}, but it is attempted to set again with #{original_thread_object_id}.")
+      raise BelongingThreadAlreadySetError.new("@belonging_thread_object_id is already set with #{@belonging_thread_object_id}, but it is attempted to set again with #{original_thread_object_id}.")
     end
 
     @belonging_thread_object_id = original_thread_object_id
