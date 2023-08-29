b92f8955853a34583b133fd478319def3f33b914
kudojp <heyjudejudejude1968@gmail.com>
2023-08-29 11:44:00 +0900

    Update address_resolve_spec

--- spec/blockings/address_resolve_spec.rb
+++ spec/blockings/address_resolve_spec.rb
@@ -3,45 +3,119 @@ require 'socket'
 require 'resolv'
 
 RSpec.describe AsyncScheduler do
-  it "resolves addresses in a Fiber.schedule block" do
-    puts "###### TODO! Confirm that times below does not improve in a liner manner."
-
-    t = Time.now
-    params = [1, 10, 100, 1000]
-
-    params.each do |num|
-      t = Time.now
-      thread = Thread.new do
-        Fiber.set_scheduler AsyncScheduler::Scheduler.new
-
-        num.times do
-          Fiber.schedule do
-            Socket.getaddrinfo("www.google.com", 443)
-            # Resolv.getaddresses("www.google.com")
-          end
+  describe "DNS resolution" do
+    def resolve_address_with_scheduler(hostname, port)
+      Thread.new do
+        scheduler = AsyncScheduler::Scheduler.new
+        Fiber.set_scheduler scheduler
+        ips = nil
+        Fiber.schedule do
+          ips = Socket.getaddrinfo(hostname, port)
         end
-      end
-      thread.join
-      puts "#{Time.now - t} seconds for resolving #{num} hostname."
+        scheduler.close
+        ips
+      end.value
+    end
+
+    it "resolves localhost successfully" do
+      expect(resolve_address_with_scheduler("localhost", 443)).to contain_exactly(
+        ["AF_INET", 443, "127.0.0.1", "127.0.0.1", 2, 1, 6],
+        ["AF_INET", 443, "127.0.0.1", "127.0.0.1", 2, 2, 17],
+        ["AF_INET", 443, "127.0.0.1", "127.0.0.1", 2, 3, 0],
+        ["AF_INET6", 443, "::1", "::1", 30, 1, 6],
+        ["AF_INET6", 443, "::1", "::1", 30, 2, 17],
+        ["AF_INET6", 443, "::1", "::1", 30, 3, 0]
+      )
+    end
+
+    it "resolves localhost successfully" do
+      expect(resolve_address_with_scheduler("localhost", 80)).to contain_exactly(
+        ["AF_INET", 80, "127.0.0.1", "127.0.0.1", 2, 1, 6],
+        ["AF_INET", 80, "127.0.0.1", "127.0.0.1", 2, 2, 17],
+        ["AF_INET", 80, "127.0.0.1", "127.0.0.1", 2, 3, 0],
+        ["AF_INET6", 80, "::1", "::1", 30, 1, 6],
+        ["AF_INET6", 80, "::1", "::1", 30, 2, 17],
+        ["AF_INET6", 80, "::1", "::1", 30, 3, 0]
+      )
+    end
+
+    it "resolves google.com successfully" do
+      address_infos = resolve_address_with_scheduler("google.com", 443)
+      ips = address_infos.map{|ai| ai[2]}.uniq
+      expect(ips.length).to eq(2) # IPv4 and IPv6
+
+      expect(address_infos).to contain_exactly(
+        ["AF_INET", 443, ips[0], ips[0], 2, 1, 6],
+        ["AF_INET", 443, ips[0], ips[0], 2, 2, 17],
+        ["AF_INET", 443, ips[0], ips[0], 2, 3, 0],
+        ["AF_INET6", 443, ips[1], ips[1], 30, 1, 6],
+        ["AF_INET6", 443, ips[1], ips[1], 30, 2, 17],
+        ["AF_INET6", 443, ips[1], ips[1], 30, 3, 0]
+      )
+    end
+
+    it "resolves google.com successfully" do
+      address_infos = resolve_address_with_scheduler("google.com", 80)
+      ips = address_infos.map{|ai| ai[2]}.uniq
+      expect(ips.length).to eq(2) # IPv4 and IPv6
+
+      # NOTE: This does not check if ips are really those of google.com.
+      expect(address_infos).to contain_exactly(
+        ["AF_INET", 80, ips[0], ips[0], 2, 1, 6],
+        ["AF_INET", 80, ips[0], ips[0], 2, 2, 17],
+        ["AF_INET", 80, ips[0], ips[0], 2, 3, 0],
+        ["AF_INET6", 80, ips[1], ips[1], 30, 1, 6],
+        ["AF_INET6", 80, ips[1], ips[1], 30, 2, 17],
+        ["AF_INET6", 80, ips[1], ips[1], 30, 3, 0]
+      )
     end
   end
 
-  it "resolves an address into IP and the result is the same as that in a synchronous manner" do
-    thread_resolving_address = Thread.new do
-      Fiber.set_scheduler AsyncScheduler::Scheduler.new
+  describe "DNS resolution performance" do
+    def resolve_address_with_scheduler(hostname, port, num_times)
+      Thread.new do
+        t = Process.clock_gettime(Process::CLOCK_MONOTONIC)
+        scheduler = AsyncScheduler::Scheduler.new
+        Fiber.set_scheduler AsyncScheduler::Scheduler.new
 
-      ips = nil
-      Fiber.schedule do
-        # ips = Socket.getaddrinfo("www.google.com", 443)
-        ips = Resolv.getaddresses("www.google.com")
-      end
-      ips
+        num_times.times do
+          Fiber.schedule do
+            Socket.getaddrinfo("www.google.com", 443)
+          end
+        end
+
+        scheduler.close
+        Process.clock_gettime(Process::CLOCK_MONOTONIC) - t
+      end.value
     end
 
-    # NOTE: This test case could be flaky, because IP addresses mapped from "www.google.com" could change.
-    # print(thread_resolving_address.value, "\n")
-    # print(Socket.getaddrinfo("www.google.com", 443), "\n")
-    require 'resolv'
-    expect(thread_resolving_address.value).to match_array Resolv.getaddresses("www.google.com")
+    def resolve_address_with_multithreads(hostname, port, num_times)
+      t = Process.clock_gettime(Process::CLOCK_MONOTONIC)
+      num_times.times do
+        Thread.new do
+          Socket.getaddrinfo("www.google.com", 443)
+        end.join
+      end
+      Process.clock_gettime(Process::CLOCK_MONOTONIC) - t
+    end
+
+    it "resolves addresses in a Fiber.schedule block" do
+      puts "👁  Confirm these things."
+      puts "- Resolving with scheduler is faster than multithreading."
+      puts "- Execution times of resolving scheduler do not improve in a liner manner."
+
+      ["localhost", "google.com"].each do |hostname|
+        puts "--------------------------"
+        puts "|   Resolve #{hostname.ljust(10)}   |"
+        puts "--------------------------"
+
+        [1, 10, 100].each do |num_times|
+          puts "## Resolving #{num_times} times:"
+          puts "Resolve with scheduler:    #{resolve_address_with_scheduler(hostname, 443, num_times)}"
+          puts "Resolve with multithreads: #{resolve_address_with_multithreads(hostname, 443, num_times)}"
+          puts
+        end
+      end
+    end
   end
 end
