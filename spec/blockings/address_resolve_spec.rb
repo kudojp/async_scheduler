a01b5fd35fa169541cc08539dd535df8e8f9338d
kudojp <heyjudejudejude1968@gmail.com>
2023-09-03 19:51:44 +0900

    Fix spec because address_info may have multiple IPv6

--- spec/blockings/address_resolve_spec.rb
+++ spec/blockings/address_resolve_spec.rb
@@ -36,29 +36,30 @@ RSpec.describe AsyncScheduler do
       # NOTE: It does not check if this IP is really google.com.
       ipv4 = address_info[0][2]
       expect(ipv4).to match(/\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}/)
-      expect(address_info).to contain_exactly(["AF_INET", 443, ipv4, ipv4, 2, 1, 6])
+      expect(address_info).to include(["AF_INET", 443, ipv4, ipv4, 2, 1, 6])
 
       address_info = resolve_address_with_scheduler("google.com", 443, family=Socket::Constants::AF_INET, socket_type=Socket::Constants::SOCK_DGRAM)
       ipv4 = address_info[0][2]
       expect(ipv4).to match(/\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}/)
-      expect(address_info).to contain_exactly(["AF_INET", 443, ipv4, ipv4, 2, 2, 17])
+      expect(address_info).to include(["AF_INET", 443, ipv4, ipv4, 2, 2, 17])
 
       address_info = resolve_address_with_scheduler("google.com", 443, family=Socket::Constants::AF_INET, socket_type=Socket::Constants::SOCK_RAW)
       ipv4 = address_info[0][2]
       expect(ipv4).to match(/\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}/)
-      expect(address_info).to contain_exactly(["AF_INET", 443, ipv4, ipv4, 2, 3, 0])
+      expect(address_info).to include(["AF_INET", 443, ipv4, ipv4, 2, 3, 0])
 
       address_info = resolve_address_with_scheduler("google.com", 443, family=Socket::Constants::AF_INET6, socket_type=Socket::Constants::SOCK_STREAM)
       ipv6 = address_info[0][2]
-      expect(address_info).to contain_exactly(["AF_INET6", 443, ipv6, ipv6, Socket::Constants::AF_INET6, 1, 6])
+      # NOTE: there could be multiple resolved IPv6 addresses.
+      expect(address_info).to include(["AF_INET6", 443, ipv6, ipv6, Socket::Constants::AF_INET6, 1, 6])
 
       address_info = resolve_address_with_scheduler("google.com", 443, family=Socket::Constants::AF_INET6, socket_type=Socket::Constants::SOCK_DGRAM)
       ipv6 = address_info[0][2]
-      expect(address_info).to contain_exactly(["AF_INET6", 443, ipv6, ipv6, Socket::Constants::AF_INET6, 2, 17])
+      expect(address_info).to include(["AF_INET6", 443, ipv6, ipv6, Socket::Constants::AF_INET6, 2, 17])
 
       address_info = resolve_address_with_scheduler("google.com", 443, family=Socket::Constants::AF_INET6, socket_type=Socket::Constants::SOCK_RAW)
       ipv6 = address_info[0][2]
-      expect(address_info).to contain_exactly(["AF_INET6", 443, ipv6, ipv6, Socket::Constants::AF_INET6, 3, 0])
+      expect(address_info).to include(["AF_INET6", 443, ipv6, ipv6, Socket::Constants::AF_INET6, 3, 0])
     end
   end
 
