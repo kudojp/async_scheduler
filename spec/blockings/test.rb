cc5735d7c52e938bb7e0524be99f57c316017d15
kudojp <heyjudejudejude1968@gmail.com>
2023-09-13 03:53:57 +0900

    Delete file checked out by mistake

--- spec/blockings/test.rb
+++
@@ -1,6 +0,0 @@
-Socket.getaddrinfo("google.com", 443, family=Socket::Constants::AF_INET, socket_type=Socket::Constants::SOCK_STREAM)
-Socket.getaddrinfo("google.com", 443, family=Socket::Constants::AF_INET, socket_type=Socket::Constants::SOCK_DGRAM)
-Socket.getaddrinfo("google.com", 443, family=Socket::Constants::AF_INET, socket_type=Socket::Constants::SOCK_RAW)
-Socket.getaddrinfo("google.com", 443, family=Socket::Constants::AF_INET6, socket_type=Socket::Constants::SOCK_STREAM)
-Socket.getaddrinfo("google.com", 443, family=Socket::Constants::AF_INET6, socket_type=Socket::Constants::SOCK_DGRAM)
-Socket.getaddrinfo("google.com", 443, family=Socket::Constants::AF_INET6, socket_type=Socket::Constants::SOCK_RAW)
